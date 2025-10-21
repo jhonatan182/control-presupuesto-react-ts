ae20c30db219dd7c84964f1702429d4ee7e77f4c
Jhonatan Vargas <73511512+jhonatan182@users.noreply.github.com>
2025-10-21 02:36:37 -0600

    diseño

--- src/components/AmountDisplay.tsx
+++ src/components/AmountDisplay.tsx
@@ -7,9 +7,12 @@ type AmountDisplayProps = {
 
 export default function AmountDisplay({ amount, label }: AmountDisplayProps) {
   return (
-    <p className="text-2xl text-blue-600 font-bold">
-      {label && `${label}: `}
+
+<p className="text-xl sm:text-2xl text-blue-600 font-bold whitespace-nowrap">
+    {label && `${label}: `}
       <span className="font-black text-black">{formatCurrency(amount)}</span>
-    </p>
+      </p>
+
+  
   );
 }
